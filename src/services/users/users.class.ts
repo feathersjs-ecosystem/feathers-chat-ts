8dc8d1558da0b668f8b07a18aedc2518e140c27d
David Luecke <daff@neyeon.com>
2020-05-06 21:24:22 -0700

    Add a display name, oAuth fallback and rename file to client.js (#43)

--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,18 +1,26 @@
+import crypto from 'crypto';
+import { Params } from '@feathersjs/feathers';
 import { Service, NedbServiceOptions } from 'feathers-nedb';
 import { Application } from '../../declarations';
-import crypto from 'crypto';
-import { Params } from 'express-serve-static-core';
 
 // The Gravatar image service
 const gravatarUrl = 'https://s.gravatar.com/avatar';
 // The size query. Our chat needs 60px images
 const query = 's=60';
+// Returns the Gravatar image for an email
+const getGravatar = (email: string) => {
+  // Gravatar uses MD5 hashes from an email address (all lowercase) to get the image
+  const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+  // Return the full avatar URL
+  return `${gravatarUrl}/${hash}?${query}`;
+}
 
 // A type interface for our user (it does not validate any data)
 interface UserData {
   _id?: string;
   email: string;
   password: string;
+  name?: string;
   avatar?: string;
   githubId?: string;
 }
@@ -24,14 +32,13 @@ export class Users extends Service<UserData> {
 
   create (data: UserData, params?: Params) {
     // This is the information we want from the user signup data
-    const { email, password, githubId } = data;
-    // Gravatar uses MD5 hashes from an email address (all lowercase) to get the image
-    const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
-    // The full avatar URL
-    const avatar = `${gravatarUrl}/${hash}?${query}`;
+    const { email, password, githubId, name } = data;
+    // Use the existing avatar image or return the Gravatar for the email
+    const avatar = data.avatar || getGravatar(email);
     // The complete user
     const userData = {
       email,
+      name,
       password,
       githubId,
       avatar
