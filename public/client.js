827ab3acf590aed29932b4b8ea48cf916dc07d56
Ray Foss <ray@vblip.com>
2022-06-30 12:33:24 -0500

    use https

--- public/client.js
+++ public/client.js
@@ -46,7 +46,7 @@ const loginHTML = `<main class="login container">
 const chatHTML = `<main class="flex flex-column">
   <header class="title-bar flex flex-row flex-center">
     <div class="title-wrapper block center-element">
-      <img class="logo" src="http://feathersjs.com/img/feathers-logo-wide.png"
+      <img class="logo" src="https://feathersjs.com/img/feathers-logo-wide.png"
         alt="Feathers Logo">
       <span class="title">Chat</span>
     </div>
